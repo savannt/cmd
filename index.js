2c36362c1b3e6cdc5337b431e2f7f402932d66d9
savannt <savant.eclipse@gmail.com>
2023-03-15 11:56:53 -0400

    v1.1.0

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+const path = require("path");
+const child_process = require("child_process");
+
 let password;
 
 const setPassword = (_password) => {
@@ -6,60 +10,44 @@ const setPassword = (_password) => {
 }
 
 const savePassword = (password) => {
-    const fs = require("fs");
-    const path = require("path");
     const p = path.join(__dirname, "password.txt");
-
     fs.writeFileSync(p, password);
 }
 
 const getPassword = () => {
-    const fs = require("fs");
-    const path = require("path");
     const p = path.join(__dirname, "password.txt");
 
-    if(!fs.existsSync(p)) return false;
+    if (!fs.existsSync(p)) return false;
     const data = fs.readFileSync(p, "utf8");
-    if(!data) return false;
+    if (!data) return false;
     return data;
 }
-
-
-const child_process = require("child_process");
-
-// Example
-// cmd(["start", "https://www.google.com"]"])
 
 const isWindows = process.platform === "win32";
 
 const parseCommand = (command, doLog = false) => {
-
     const _parseCommand = (command) => {
-        if(command.includes("sudo")) {
-            if(!password) {
+        if (command.includes("sudo")) {
+            if (!password) {
                 const savedPassword = getPassword();
-                if(!savedPassword) {
+                if (!savedPassword) {
                     throw new Error("@cmd: Password not set");
                 } else {
-                    if(doLog) console.log("@cmd: Using saved password");
+                    if (doLog) console.log("@cmd: Using saved password");
                     password = savedPassword;
                 }
             }
-            // replace first sudo occurence with `sudo -S <<< ${password}`
-            // any other sudo occurences will be replaced with `sudo -S`
             const sudoReplacement = `echo ${password} | sudo -S`;
             const sudoReplacement2 = `sudo -S`;
             command = command.replace("sudo", sudoReplacement);
-            // make sure we don't replace the first sudo occurence
             command = command.replace(/sudo(?!\s+-S)/g, sudoReplacement2);
         }
         return command;
     }
 
-    // if command is obj
-    if(typeof command === "object") {
-        if(command.linux && !isWindows) command = _parseCommand(command.linux);
-        else if(command.windows && isWindows) command = _parseCommand(command.windows);
+    if (typeof command === "object") {
+        if (command.linux && !isWindows) command = _parseCommand(command.linux);
+        else if (command.windows && isWindows) command = _parseCommand(command.windows);
         else {
             console.log("Invalid command object");
             return;
@@ -70,42 +58,48 @@ const parseCommand = (command, doLog = false) => {
     return command;
 }
 
-
 const cmdSync = (command, doLog = false) => {
     command = parseCommand(command, doLog);
 
     const commandStr = command;
-    if(doLog) console.log("@cmd: executing cmd string", "\"" + commandStr + "\"");
+    if (doLog) console.log("@cmd: executing cmd string", "\"" + commandStr + "\"");
 
     try {
         const stdout = child_process.execSync(commandStr);
-        if(!stdout) return true;
+        if (!stdout) return true;
         return stdout;
     } catch (err) {
         return false;
     }
 }
 
-const cmd = (command, doLog = false) => {
+const cmd = async (command, options = {}) => {
+    const { doLog = false, detached = false } = options;
     command = parseCommand(command, doLog);
 
     return new Promise((resolve) => {
-        function reject (...args) {
-            if(doLog) console.log(...args);
+        function reject(...args) {
+            if (doLog) console.log(...args);
             resolve(args[2].toString());
         }
-        
+
         const commandStr = command;
-        if(doLog) console.log("@cmd: executing cmd string", "\"" + commandStr + "\"");
-        child_process.exec(commandStr, (error, stdout, stderr) => {
-            if(error) reject("@cmd: An error occured whilst executing", `"${commandStr}"`, error);
-            if(stderr) {
-                if(doLog) console.log("@cmd: An stderr occured whilst executing", `"${commandStr}"`, stderr);
+        if (doLog) console.log("@cmd: executing cmd string", "\"" + commandStr + "\"");
+
+        const child = child_process.exec(commandStr, (error, stdout, stderr) => {
+            if (error) reject("@cmd: An error occurred whilst executing", `"${commandStr}"`, error);
+            if (stderr) {
+                if (doLog) console.log("@cmd: An stderr occurred whilst executing", `"${commandStr}"`, stderr);
                 return resolve(stderr);
             }
-            if(!stdout) resolve(true);
+            if (!stdout) resolve(true);
             resolve(stdout);
         });
+
+        if (detached) {
+            child.unref();
+            resolve(true);
+        }
     });
 }
 
